726856d5e9984c1d52355aed8eb335ddd954f524
Iago Corrêa <iago.gc.eliote@gmail.com>
2016-10-18 02:20:44 -0200

    Ação de clique na tela inicial e botão de adição

---
+++ Application/app/src/main/java/com/example/iago/application/DisplayCharacters.java
@@ -0,0 +1,17 @@
+package com.example.iago.application;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Iago on 18/10/2016.
+ */
+public class DisplayCharacters extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_characters);
+    }
+
+}
--- Application/app/src/main/java/com/example/iago/application/MainActivity.java
+++ Application/app/src/main/java/com/example/iago/application/MainActivity.java
@@ -2,12 +2,37 @@ package com.example.iago.application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.content.Context;
+import android.content.Intent;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView obj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        obj = (ImageView)findViewById(R.id.cover);
+        obj.setOnClickListener(new  View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),DisplayCharacters.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
