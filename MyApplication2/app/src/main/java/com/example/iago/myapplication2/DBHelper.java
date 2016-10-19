55aa98be2a671314eecdf9b0fc7869f7385863d9
DarkDjango <dark.django@hotmail.com>
2016-10-18 22:39:31 -0200

    DBHelper Atualizado #2
    DB Helper:
    - Removida diversas redundâncias no código.
    - Criada tabela de raças.
    - Removido métodos individuais de getAll; agora existe apenas um
    "getAll(string, int)" cujo primeiro argumento define o que vai buscar
    (personagens, classes, raças).
    
    Tabelas do Excel:
    - Adicionada coluna com as descrições .

--- MyApplication2/app/src/main/java/com/example/iago/myapplication2/DBHelper.java
+++ MyApplication2/app/src/main/java/com/example/iago/myapplication2/DBHelper.java
@@ -23,17 +23,24 @@ public class DBHelper extends SQLiteOpenHelper{
     public static final String CHAR_COLUMN_INT = "inteligência";
     public static final String CHAR_COLUMN_SAB = "sabedoria";
     public static final String CHAR_COLUMN_CAR = "carisma";
+    public static final String CHAR_COLUMN_RAÇA = "raçaid";
 
     public static final String CLASS_TABLE_NAME = "classes";
     public static final String CLASS_COLUMN_ID = "classid";
     public static final String CLASS_COLUMN_NAME = "classnome";
-    public static final String CLASS_TABLE_BBA = "bbaclasse";
-    public static final String CLASS_TABLE_PVS = "pvsclasse";
-    public static final String CLASS_TABLE_PERÍCIAS = "perclasse";
-    public static final String CLASS_TABLE_PRESTÍGIO = "prestígio";
-    public static final String CLASS_TABLE_NÍVEL = "nívelmax";
-    public static final String CLASS_TABLE_ORIGEM = "classorigem";
+    public static final String CLASS_COLUMN_BBA = "bbaclasse";
+    public static final String CLASS_COLUMN_PVS = "pvsclasse";
+    public static final String CLASS_COLUMN_PERÍCIAS = "perclasse";
+    public static final String CLASS_COLUMN_PRESTÍGIO = "prestígio";
+    public static final String CLASS_COLUMN_NÍVEL = "nívelmax";
+    public static final String CLASS_COLUMN_ORIGEM = "classorigem";
+    public static final String CLASS_COLUMN_DESCRIPTION = "classdescrição";
 
+    public static final String RACE_TABLE_NAME = "raças";
+    public static final String RACE_COLUMN_ID = "raçaid";
+    public static final String RACE_COLUMN_NAME = "raçanome";
+    public static final String RACE_COLUMN_ORIGEM = "raçanome";
+    public static final String RACE_COLUMN_DESCRIPTION = "raçadescrição";
     private HashMap hp;
 
     public DBHelper(Context context) {
@@ -51,6 +58,10 @@ public class DBHelper extends SQLiteOpenHelper{
                 "create table classes " +
                         "(classid integer primary key, classnome text, classenome text, bbaclasse integer, pvsclasse integer, perclasse integer, prestígio boolean, nívelmax integer, classorigem text)"
         );
+        db.execSQL(
+                "create table raças " +
+                        "(raçaid integer primary key, raçanome text, raçaorigem text, raçadescrição text)"
+        );
     }
 
     @Override
@@ -58,37 +69,49 @@ public class DBHelper extends SQLiteOpenHelper{
         // TODO Auto-generated method stub
         db.execSQL("DROP TABLE IF EXISTS personagens");
         db.execSQL("DROP TABLE IF EXISTS classes");
+        db.execSQL("DROP TABLE IF EXISTS raças");
         onCreate(db);
     }
 
-    public boolean insertPersonagem (String name, int força, int destreza, int constituição, int inteligência, int sabedoria, int carisma){
+    public boolean insertPersonagem (String name, int força, int destreza, int constituição, int inteligência, int sabedoria, int carisma, int raçaid){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("força", força);
-        contentValues.put("destreza", destreza);
-        contentValues.put("constituição", constituição);
-        contentValues.put("inteligência", inteligência);
-        contentValues.put("sabedoria", sabedoria);
-        contentValues.put("carisma", carisma);
-        db.insert("personagens", null, contentValues);
+        contentValues.put(CHAR_COLUMN_NAME, name);
+        contentValues.put(CHAR_COLUMN_FOR, força);
+        contentValues.put(CHAR_COLUMN_DES, destreza);
+        contentValues.put(CHAR_COLUMN_CON, constituição);
+        contentValues.put(CHAR_COLUMN_INT, inteligência);
+        contentValues.put(CHAR_COLUMN_SAB, sabedoria);
+        contentValues.put(CHAR_COLUMN_CAR, carisma);
+        contentValues.put(CHAR_COLUMN_RAÇA, raçaid);
+        db.insert(CHAR_TABLE_NAME, null, contentValues);
         return true;
     }
 
-    public boolean insertClasse(String name, int bba, int pvs, int pericias, boolean prestígio, int nivelmax, String origem){
+    public boolean insertClasse(String name, int bba, int pvs, int pericias, boolean prestígio, int nivelmax, String origem, String descrição){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("bbaclasse",bba);
-        contentValues.put("pvsclasse",pvs);
-        contentValues.put("perclasse",pericias);
-        contentValues.put("prestígio",prestígio);
-        contentValues.put("nívelmax",nivelmax);
-        contentValues.put("classorigem",origem);
-        db.insert("classes", null, contentValues);
+        contentValues.put(CLASS_COLUMN_NAME, name);
+        contentValues.put(CLASS_COLUMN_BBA,bba);
+        contentValues.put(CLASS_COLUMN_PVS,pvs);
+        contentValues.put(CLASS_COLUMN_PERÍCIAS,pericias);
+        contentValues.put(CLASS_COLUMN_PRESTÍGIO,prestígio);
+        contentValues.put(CLASS_COLUMN_NÍVEL,nivelmax);
+        contentValues.put(CLASS_COLUMN_ORIGEM,origem);
+        contentValues.put(CLASS_COLUMN_DESCRIPTION,descrição);
+        db.insert(CLASS_TABLE_NAME, null, contentValues);
         return true;
     }
 
+    public boolean insertRaça(String name, String origem, String descrição){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(RACE_COLUMN_NAME, name);
+        contentValues.put(RACE_COLUMN_ORIGEM,origem);
+        contentValues.put(RACE_COLUMN_DESCRIPTION,descrição);
+        db.insert(RACE_TABLE_NAME, null, contentValues);
+        return true;
+    }
     public Cursor getData(String table, int id){
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor res = null;
@@ -96,7 +119,25 @@ public class DBHelper extends SQLiteOpenHelper{
             res =  db.rawQuery( "select * from personagens where id="+id+"", null );
         else if  (table == "classe")
             res =  db.rawQuery( "select * from classes where id="+id+"", null );
+        else if  (table == "raça")
+            res =  db.rawQuery( "select * from raças where id="+id+"", null );
         return res;
+    }
+
+    public String getDescr(String table, int id) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor res = null;
+        String desc;
+        if (table == "classe") {
+            res = db.rawQuery("select * from classes where id=" + id + "", null);
+            desc = res.getString(res.getColumnIndex(DBHelper.CLASS_COLUMN_DESCRIPTION));
+        } else if (table == "raça") {
+            res = db.rawQuery("select * from raças where id=" + id + "", null);
+            desc = res.getString(res.getColumnIndex(DBHelper.RACE_COLUMN_DESCRIPTION));
+        }
+          else
+            desc = "ERRO: Descrição não-encontrada.";
+        return desc;
     }
 
     public int mod (String atributo, int id) {
@@ -127,6 +168,8 @@ public class DBHelper extends SQLiteOpenHelper{
             numRows = (int) DatabaseUtils.queryNumEntries(db, CHAR_TABLE_NAME);
         else if  (table == "classe")
             numRows = (int) DatabaseUtils.queryNumEntries(db, CLASS_TABLE_NAME);
+        else if  (table == "raça")
+            numRows = (int) DatabaseUtils.queryNumEntries(db, RACE_TABLE_NAME);
         return numRows;
     }
 
@@ -134,50 +177,47 @@ public class DBHelper extends SQLiteOpenHelper{
     public boolean updatePersonagem (Integer id, String name, int força, int destreza, int constituição, int inteligência, int sabedoria, int carisma){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put("name", name);
-        contentValues.put("força", força);
-        contentValues.put("destreza", destreza);
-        contentValues.put("constituição", constituição);
-        contentValues.put("inteligência", inteligência);
-        contentValues.put("sabedoria", sabedoria);
-        contentValues.put("carisma", carisma);
-        db.update("personagens", contentValues, "id = ? ", new String[] { Integer.toString(id) } );
+        contentValues.put(CHAR_COLUMN_NAME, name);
+        contentValues.put(CHAR_COLUMN_FOR, força);
+        contentValues.put(CHAR_COLUMN_DES, destreza);
+        contentValues.put(CHAR_COLUMN_CON, constituição);
+        contentValues.put(CHAR_COLUMN_INT, inteligência);
+        contentValues.put(CHAR_COLUMN_SAB, sabedoria);
+        contentValues.put(CHAR_COLUMN_CAR, carisma);
+        db.update(CHAR_TABLE_NAME, contentValues, "id = ? ", new String[] { Integer.toString(id) } );
         return true;
     }
 
     public Integer deletePersonagem (Integer id){
         SQLiteDatabase db = this.getWritableDatabase();
-        return db.delete("personagens",
+        return db.delete(CHAR_TABLE_NAME,
                 "id = ? ",
                 new String[] { Integer.toString(id) });
     }
 
-    public ArrayList<String> getAllPersonagens(){
+    public ArrayList<String> getAll(String table){
         ArrayList<String> array_list = new ArrayList<String>();
-
-        //hp = new HashMap();
         SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from personagens", null );
-        res.moveToFirst();
-
-        while(res.isAfterLast() == false){
-            array_list.add(res.getString(res.getColumnIndex(CHAR_COLUMN_NAME)));
-            res.moveToNext();
+        Cursor res = null;
+        String column = null;
+        if  (table == "personagens") {
+            res = db.rawQuery("select * from personagens", null);
+            column = CHAR_COLUMN_NAME;
         }
-        return array_list;
-    }
-
-    public ArrayList<String> getAllClasses(){
-        ArrayList<String> array_list = new ArrayList<String>();
-
-        //hp = new HashMap();
-        SQLiteDatabase db = this.getReadableDatabase();
-        Cursor res =  db.rawQuery( "select * from classes", null );
-        res.moveToFirst();
-
-        while(res.isAfterLast() == false){
-            array_list.add(res.getString(res.getColumnIndex(CLASS_COLUMN_NAME)));
-            res.moveToNext();
+        else if  (table == "classes") {
+            res = db.rawQuery("select * from classes", null);
+            column = CLASS_COLUMN_NAME;
+        }
+        else if  (table == "raças") {
+            res = db.rawQuery("select * from raças", null);
+            column = RACE_COLUMN_NAME;
+        }
+        if (res != null) {
+            res.moveToFirst();
+            while (res.isAfterLast() == false) {
+                array_list.add(res.getString(res.getColumnIndex(column)));
+                res.moveToNext();
+            }
         }
         return array_list;
     }
