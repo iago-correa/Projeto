7d75f2e8cc1444b2c9b6922546b3625dea01e26c
Iago Corrêa <iago.gc.eliote@gmail.com>
2016-11-14 22:40:48 -0200

    Retificação de erro meu
    Depois disso não te atrapalho mais mexendo no dbhelper

--- Application/app/src/main/java/com/example/iago/application/DBHelper.java
+++ Application/app/src/main/java/com/example/iago/application/DBHelper.java
@@ -337,10 +337,10 @@ public class DBHelper extends SQLiteOpenHelper{
         Cursor res = null;
         String desc;
         if (table == "classe") {
-            res = db.rawQuery("select classdescricao from classes where nome=" + nome + "", null);
+            res = db.rawQuery("select classdescricao from classes where classnome=" + nome + "", null);
             desc = res.getString(res.getColumnIndex(DBHelper.CLASS_COLUMN_DESCRIPTION));
         } else if (table == "raca") {
-            res = db.rawQuery("select racadescricao from racas where nome=" + nome + "", null);
+            res = db.rawQuery("select racadescricao from racas where racanome=" + nome + "", null);
             desc = res.getString(res.getColumnIndex(DBHelper.RACE_COLUMN_DESCRIPTION));
         }
         else
